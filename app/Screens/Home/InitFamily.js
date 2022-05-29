3a384a7beff9d133e341b4c40ec5b1beef2d350e
SONKENG MBOGNING Brice <cruzorbladex@gmail.com>
2022-05-29 13:55:45 +0100

    Now user can initiate a family, but not launch the otp verification

--- app/Screens/Home/InitFamily.js
+++ app/Screens/Home/InitFamily.js
@@ -1,29 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image, Modal, TextInput } from 'react-native';
 import { useNavigation } from 'react-navigation-hooks';
 
 export default function InitFamily(){
   const navigation = useNavigation();
   const [show, setShow] = useState(false);
-  const [emails, setEmails] = useState('');
+  const [phoneInvitee, setPhoneInvitee] = useState('');
+  const [family, setFamily] = useState(JSON.parse(navigation.state.params.familyObj))
 
   const sendInvitation = () => {
     setShow(false);
   }
-
+  console.log('Family: address', family.address);
   const putModal = () => {
     return(
       <Modal animationType={"slide"} transparent={true} visible={show} onRequestClose={() => setShow(false) }>
         <View style = {styles.modalwrap}>
           <View style = {styles.modal}>
-            <Text style={{ color: '#424242' }}>Add Email</Text>
+            <Text style={{ color: '#424242', marginBottom:5 }}>Add phone number</Text>
             <TextInput
-              value={emails}
-              onChangeText={e => setEmails(e)}
-              placeholder={'Email Address'}
-              keyboardType='email-address'
+              value={phoneInvitee}
+              onChangeText={e => setPhoneInvitee(e)}
+              placeholder={'Phone number'}
+              keyboardType='phone-pad'
               multiline
-              style={{ height: 100, color: 'green', textAlignVertical: 'top', padding: 6, alignItems: 'flex-start', width: '100%', borderColor: '#15B715', borderWidth: 1, marginBottom: 15, borderRadius: 6, marginTop: 5 }}
+              style={{ color: 'green', textAlignVertical: 'center', padding: 6, alignItems: 'flex-start', width: '100%', borderColor: '#15B715', borderWidth: 1, marginBottom: 15, borderRadius: 6, marginTop: 5 }}
             />
             <TouchableOpacity onPress={() => sendInvitation()} style={styles.btn}>
               <Text style={{ color: 'white' }}>Send Invitation</Text>
@@ -33,7 +34,9 @@ export default function InitFamily(){
       </Modal>
     )
   }
-
+  let members = family.members;
+  members.push(15);
+  
   return(
     <View style={styles.main}>
       <View style={styles.header}>
@@ -41,22 +44,22 @@ export default function InitFamily(){
           <Image source={require('./../../Assets/icons/back.png')} style={styles.back} />
         </TouchableOpacity>
         <View style={{ flexDirection: 'row', flex: 1, justifyContent: 'center', marginLeft: -20 }}>
-          <Text style={{ color: '#000000' }}>Name of family</Text>
+          <Text style={{ color: '#000000' }}>{family.family_name}</Text>
         </View>
       </View>
       <View style={styles.body}>
         <View style={styles.top}>
           <Image source={require('./../../Assets/family.jpg')} style={styles.img} />
           <View style={styles.info}>
-            <Text numberOfLines={1} style={{ fontSize: 18, fontWeight: 'bold', color: 'black' }}>Name of fmaily</Text>
+            <Text numberOfLines={1} style={{ fontSize: 18, fontWeight: 'bold', color: 'black' }}>{family.family_name}</Text>
             <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 5 }}>
               <Image source={require('./../../Assets/icons/location.png')} style={styles.icon} />
-              <Text numberOfLines={1} style={{ marginLeft: 5, color: '#424242' }}>Buea, Cameroon</Text>
+              <Text numberOfLines={1} style={{ marginLeft: 5, color: '#424242' }}>{family.address}</Text>
             </View>
           </View>
         </View>
         <View style={styles.section}>
-          <Text style={{ color: '#aaa' }}>No family member yet</Text>
+          <Text style={{ color: '#aaa' }}>{!family.members.lenght ? 'No family member yet': 'Some family members'}</Text>
           <Text style={{ color: '#424242' }}>Invite new members to your family</Text>
         </View>
         <TouchableOpacity onPress={() => setShow(true)} style={styles.sendbtn}>
